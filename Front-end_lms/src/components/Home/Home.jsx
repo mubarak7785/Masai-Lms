0a019d8ae6253f0a719f6170c4695a77fa5395ee
mubarak7785 <mubarakjinabade07@gmail.com>
2022-09-08 09:48:43 +0530

    updated the ui of lecture page and fetched the data from backend

--- Front-end_lms/src/components/Home/Home.jsx
+++ Front-end_lms/src/components/Home/Home.jsx
@@ -14,60 +14,65 @@ export const Home = () => {
         </div>
       </div>
       <div className="today-content">
-            <div className="box">
-                <h3>9:00 AM</h3>
-                <h3>SCRUM</h3>
-
-            </div>
-            <div className="details">
-                <p>Scrum 13.08.2022</p>
-                <p> <span>Nrupul</span> Starting at <span>9:00 AM</span></p>
-                <button>live</button>
-            </div>
-            <p>Details</p>
+        <div className="box">
+          <h3>9:00 AM</h3>
+          <h3>SCRUM</h3>
+        </div>
+        <div className="details">
+          <p>Scrum 13.08.2022</p>
+          <p>
+            {" "}
+            <span>Nrupul</span> Starting at <span>9:00 AM</span>
+          </p>
+          <button>live</button>
+        </div>
+        <p>Details</p>
       </div>
       <div className="today-content">
-            <div className="box">
-                <h3>9:00 AM</h3>
-                <h3>SCRUM</h3>
-
-            </div>
-            <div className="details">
-                <p>Scrum 12.08.2022</p>
-                <p> <span>Nrupul</span> Starting at <span>9:00 AM</span></p>
-                <button>live</button>
-            </div>
-            <p>Details</p>
+        <div className="box">
+          <h3>9:00 AM</h3>
+          <h3>SCRUM</h3>
+        </div>
+        <div className="details">
+          <p>Scrum 12.08.2022</p>
+          <p>
+            {" "}
+            <span>Nrupul</span> Starting at <span>9:00 AM</span>
+          </p>
+          <button>live</button>
+        </div>
+        <p>Details</p>
       </div>
       <div className="today-content">
-            <div className="box">
-                <h3>9:00 AM</h3>
-                <h3>SCRUM</h3>
-
-            </div>
-            <div className="details">
-                <p>Scrum 11.08.2022</p>
-                <p> <span>Nrupul</span> Starting at <span>9:00 AM</span></p>
-                <button>live</button>
-            </div>
-            <p>Details</p>
+        <div className="box">
+          <h3>9:00 AM</h3>
+          <h3>SCRUM</h3>
+        </div>
+        <div className="details">
+          <p>Scrum 11.08.2022</p>
+          <p>
+            {" "}
+            <span>Nrupul</span> Starting at <span>9:00 AM</span>
+          </p>
+          <button>live</button>
+        </div>
+        <p>Details</p>
       </div>
       <div className="today-content">
-            <div className="box">
-                <h3>9:00 AM</h3>
-                <h3>SCRUM</h3>
-
-            </div>
-            <div className="details">
-                <p>Scrum 10
-                  
-                  .08.2022</p>
-                <p> <span>Nrupul</span> Starting at <span>9:00 AM</span></p>
-                <button>live</button>
-            </div>
-            <p>Details</p>
+        <div className="box">
+          <h3>9:00 AM</h3>
+          <h3>SCRUM</h3>
+        </div>
+        <div className="details">
+          <p>Scrum 10 .08.2022</p>
+          <p>
+            {" "}
+            <span>Nrupul</span> Starting at <span>9:00 AM</span>
+          </p>
+          <button>live</button>
+        </div>
+        <p>Details</p>
       </div>
-
     </div>
   );
 };
--- Front-end_lms/src/components/Lectures/Letures.jsx
+++ Front-end_lms/src/components/Lectures/Letures.jsx
@@ -1,37 +1,47 @@
+import { useState } from "react"
+import { useEffect } from "react"
 import { Nabvar } from "../Navbar/Navbar"
 import "./Lectures.css"
 export const Lectures=()=>{
+
+    const [data,setData]=useState([])
+
+
+    useEffect(()=>{
+        fetch("https://lmsofmasai.herokuapp.com/lectures").then(res=>res.json()).then(data=>setData(data))
+    },[])
+
+
+
     return(
         <div>
             <Nabvar/>
             <div className="lec">
                 <p>Lectures</p>
             </div>
-            <div className="lec-content">
-                <div className="lec-sub">
-                    <p>Thrive Regular Standup [week-8]</p>
-                    <p><span>Swanand Kadam</span>  Scheduled <span>Coading</span>  at <span>10 Aug, 22 </span> - <span> 12:30 pm</span></p>
+            {data.map((e)=>{
+                return(
+                    <div className="lec-content">
+                    <div className="lec-sub">
+                        <p>{e.title}<span><button>live</button></span> </p>
+                        <p><span>{e.Instructor}</span>  Scheduled <span>{e.type}</span>  at <span>{e.date}</span> - <span>{e.time}</span></p>
+                        
+                    </div>
+                    <button>Absent</button>
                 </div>
-                <button>Absent</button>
-            </div>
+                )
+            })}
             <div className="lec-content">
                 <div className="lec-sub">
-                    <p>scrum 09.08.2022</p>
+                    <p>scrum 09.08.2022  <span><button>live</button></span> </p>
                     <p><span>Nrupul</span>  Scheduled <span>Scrum</span>  at <span>9 Aug, 22 </span> - <span> 9:00 am</span></p>
                 </div>
                 <button>Absent</button>
             </div>
             <div className="lec-content">
                 <div className="lec-sub">
-                    <p>Weekly connect call</p>
+                    <p>Weekly connect call  <span><button>live</button></span> </p>
                     <p><span>Yogesh</span>  Scheduled <span>Scrum</span>  at <span>7 Aug, 22 </span> - <span> 12:00 pm</span></p>
-                </div>
-                <button>Absent</button>
-            </div>
-            <div className="lec-content">
-                <div className="lec-sub">
-                    <p>Thrive Regular Standup [week-7]</p>
-                    <p><span>Swanand Kadam</span>  Scheduled <span>Scrum</span>  at <span>1 Aug, 22 </span> - <span> 11:00 am</span></p>
                 </div>
                 <button>Absent</button>
             </div>
