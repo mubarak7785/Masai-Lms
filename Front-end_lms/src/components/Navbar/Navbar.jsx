651d61cd7606bd78323c97b75bfd8cd6c8d48529
mubarak7785 <mubarakjinabade07@gmail.com>
2022-09-08 13:04:16 +0530

    removed routes for quiz and course section

--- Front-end_lms/src/components/Navbar/Navbar.jsx
+++ Front-end_lms/src/components/Navbar/Navbar.jsx
@@ -7,8 +7,8 @@ export const Nabvar=()=>{
                 <Link to="/home"><img src="https://www.masaischool.com/img/navbar/logo.svg" alt="" /></Link>
                 <Link to="/lectures"><p>Lectures</p></Link>
                 <Link to="/assignments"><p>Assignments</p></Link>
-                <Link to="/courses"> <p>Courses</p></Link>
-                <Link to="/quizzes"> <p>Quizzes</p></Link>
+                <Link to=""> <p>Courses</p></Link>
+                <Link to=""> <p>Quizzes</p></Link>
                 <Link to="/admin">  <p>Admin</p></Link>
                 <Link to="/signup"> <p>Register</p></Link>
                <Link to="/"> <p>Sign In</p></Link> 
