0e1685e03502e46a9615d29f8818dbc2e504e6fa
mubarak7785 <mubarakjinabade07@gmail.com>
2022-09-08 12:50:06 +0530

    Added lectures section in Admin page

--- Front-end_lms/src/components/Admin/Admin.jsx
+++ Front-end_lms/src/components/Admin/Admin.jsx
@@ -5,11 +5,18 @@ import { useState } from "react";
 
 export const Admin = () => {
   const [admindata, setAdmindata] = useState({});
+  const [lecturedata ,setLecturedata]=useState({});
   const [data,setData]=useState({})
+  const [ldata,setLdata]=useState({})
 
   const handleChange = (e) => {
     const { id, value } = e.target;
     setAdmindata({ ...admindata, [id]: value });
+  };
+
+  const handleChange1 = (e) => {
+    const { id, value } = e.target;
+    setLecturedata({ ...lecturedata, [id]: value });
   };
 
   const postdata=(e)=>{
@@ -26,13 +33,26 @@ export const Admin = () => {
     if(data){
       alert("Assignment is added !")
     }
-    else{
-      alert("ALERTTTTTTT")
+    
+  }
+
+  const postdata1=(e)=>{
+    e.preventDefault();
+    fetch("https://lmsofmasai.herokuapp.com/lectures",{
+      method:"POST",
+      body : JSON.stringify(lecturedata),
+      headers: { "content-type": "application/json" },
+    })
+    .then(Response=>Response.json())
+    .then((data) => {
+      setLdata(data);
+    })
+    if(data){
+      alert("Lecture is added !")
     }
     
   }
 
-  console.log(admindata);
 
   return (
     <div>
@@ -40,8 +60,7 @@ export const Admin = () => {
       <div className="lec">
         <p>Admin's Page</p>
       </div>
-
-      <h2>Add lectures</h2>
+      <h2>Add  Assignments</h2>
       <div className="main-admin-div">
         <div className="title-div">
           <p>Title</p>
@@ -100,6 +119,67 @@ export const Admin = () => {
           </div>
         </div>
         <button onClick={postdata}>Add</button>
+      </div>
+      <br />
+      <h2>Add  Lectures</h2>
+      <div className="main-admin-div">
+        <div className="title-div">
+          <p>Title</p>
+          <input type="text" id="title" onChange={handleChange1} />
+        </div>
+        <div className="mid-div">
+          <div>
+            <p>Category</p>
+            <select name="" id="category" onChange={handleChange1}>
+              <option value="">Choose</option>
+              <option value="Scrum">Scrum</option>
+              <option value="General">General</option>
+              <option value="Skillathon">Skillathon</option>
+              <option value="Coading">Coading</option>
+            </select>
+          </div>
+          <div>
+            <p>TYPE</p>
+            <select name="" id="type" onChange={handleChange1}>
+              <option value="">Choose</option>
+              <option value="Live">Live</option>
+              <option value="Video">Video</option>
+              <option value="Reading">Reading</option>
+              
+            </select>
+          </div>
+          <div>
+            <p>Instructor</p>
+            <select name="" id="Instructor" onChange={handleChange1}>
+              <option value="">Choose</option>
+              <option value="Swanand">Swanand</option>
+              <option value="Rahul">Rahul</option>
+              <option value="Sobin O">Sobin O</option>
+              <option value="Nrupul">Nrupul</option>
+              <option value="Vishakha">Vishakha</option>
+              <option value="Albert">Albert</option>
+            </select>
+          </div>
+        </div>
+        <div className="mid-div">
+          <div>
+            <p>Scheduled</p>
+            <input type="date" id="date" onChange={handleChange1} />
+          </div>
+          <div>
+            <p>Optional</p>
+            <select name="" id="optional" onChange={handleChange1}>
+              <option value="">Choose</option>
+              <option value="No">No</option>
+              <option value="Yes">Yes</option>
+            </select>
+          </div>
+          <div>
+            <p>Time</p>
+            <input type="time" id="time" onChange={handleChange1} />
+          </div>
+        </div>
+        <button onClick={postdata1}>Add</button>
       </div>
     </div>
   );
